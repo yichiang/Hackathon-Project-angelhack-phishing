75162810a3c902807b3a77ef3cfec77bd2934754
yichiang <yi_chiang@Outlook.com>
2017-07-16 12:01:53 -0700

    add code

--- chromeplugin/background.js
+++ chromeplugin/background.js
@@ -8,9 +8,9 @@ chrome.tabs.onCreated.addListener(function(tab) {
 
 
 
-  // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  //   if(tab.url == 'http://swordfishhackathon.s3-website.us-east-2.amazonaws.com/')
-  //   {
-  //     alert('We believe this url is unsafe and attempting to impersonate bankofamerica.com');
-  //   }
-  // });
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if(tab.url == 'http://swordfishhackathon.s3-website.us-east-2.amazonaws.com/')
+    {
+      alert('We believe this url is unsafe and attempting to impersonate bankofamerica.com');
+    }
+  });
