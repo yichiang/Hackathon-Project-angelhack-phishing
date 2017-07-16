f217f32106a6a0bb9365bc851e82e5842b598e64
yichiang <yi_chiang@Outlook.com>
2017-07-16 11:49:59 -0700

    add code

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import { Accordion, Grid, Image, Card, Icon, Input, Menu, Segment } from 'semantic-ui-react'
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
-// var MapGL = require('react-map-gl');
+import { Accordion, Grid, Image, Card, Icon, Input, Menu, Segment, Table, Checkbox, Button } from 'semantic-ui-react'
+import {PieChart, Pie, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import './App.css';
 import {links} from './linkData';
 // var ReactHighmaps = require('react-highcharts/ReactHighmaps.src');
 // var Highlight = require('react-highlight');
 // var ReactDOM = require('react-dom');
 // var maps = require('./mapdata/europe'
+const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
+                  {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
+
+const data02 = [{name: 'Group A', value: 2400}, {name: 'Group B', value: 4567},
+                  {name: 'Group C', value: 1398}, {name: 'Group D', value: 9800},
+                  {name: 'Group E', value: 3908}, {name: 'Group F', value: 4800}];
+
 const panels = [
 {
   title: `Immediate Attention Required`,
@@ -143,8 +150,8 @@ class Home extends Component {
                               <Menu.Header>Support</Menu.Header>
 
                               <Menu.Menu>
-                                <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
-                                  E-mail Support
+                                <Menu.Item name='admin' active={activeItem === 'admin'} onClick={this.handleItemClick}>
+                                  Users
                                 </Menu.Item>
 
                                 <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
@@ -197,6 +204,65 @@ class Home extends Component {
               </Grid>
             </div>
             }
+            {activeItem === 'admin' &&
+            <Segment>
+              <Table compact celled definition>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell />
+          <Table.HeaderCell>Name</Table.HeaderCell>
+          <Table.HeaderCell>Registration Date</Table.HeaderCell>
+          <Table.HeaderCell>E-mail address</Table.HeaderCell>
+          <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell collapsing>
+            <Checkbox slider />
+          </Table.Cell>
+          <Table.Cell>Yi</Table.Cell>
+          <Table.Cell>Feb 02, 2017</Table.Cell>
+          <Table.Cell>yi@hackathon.com</Table.Cell>
+          <Table.Cell>No</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell collapsing>
+            <Checkbox slider />
+          </Table.Cell>
+          <Table.Cell>Zack</Table.Cell>
+          <Table.Cell>Feb 11, 2017</Table.Cell>
+          <Table.Cell>zack@hackathon.com</Table.Cell>
+          <Table.Cell>Yes</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell collapsing>
+            <Checkbox slider />
+          </Table.Cell>
+          <Table.Cell>Black</Table.Cell>
+          <Table.Cell>May 11, 2017</Table.Cell>
+          <Table.Cell>black@yahoo.com</Table.Cell>
+          <Table.Cell>Yes</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+
+      <Table.Footer fullWidth>
+        <Table.Row>
+          <Table.HeaderCell />
+          <Table.HeaderCell colSpan='4'>
+            <Button floated='right' icon labelPosition='left' primary size='small'>
+              <Icon name='user' /> Add User
+            </Button>
+            <Button size='small'>Approve</Button>
+            <Button disabled size='small'>Approve All</Button>
+          </Table.HeaderCell>
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+            </Segment>
+
+            }
             {activeItem === 'report'&&
             // <Segment>
               <Grid celled>
@@ -225,6 +291,17 @@ class Home extends Component {
                 </Grid>
             // </Segment>
 
+            }
+            {activeItem === 'warning' &&
+
+
+              <Segment>
+                <PieChart width={800} height={400}>
+                  <Pie isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label/>
+                  {/* <Pie data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/> */}
+                  <Tooltip/>
+               </PieChart>
+              </Segment>
             }
 
 
