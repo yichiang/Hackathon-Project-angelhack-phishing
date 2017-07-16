efadda86b39ec87b43f03152996ff458f645f573
yichiang <yi_chiang@Outlook.com>
2017-07-15 17:44:18 -0700

    add code

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-import { Icon, Input, Menu, Segment } from 'semantic-ui-react'
+import { Accordion, Grid, Image, Card, Icon, Input, Menu, Segment } from 'semantic-ui-react'
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+// var MapGL = require('react-map-gl');
+import './App.css';
+import {links} from './linkData';
+// var ReactHighmaps = require('react-highcharts/ReactHighmaps.src');
+// var Highlight = require('react-highlight');
+// var ReactDOM = require('react-dom');
+// var maps = require('./mapdata/europe'
+const panels = [
+{
+  title: `Immediate Attention Required`,
+  color: 'red',
+  className: 'green',
+ content: 'immediate attention required',
+},
+{
+  title: `Review required`,
+   color: 'orange',
+   className: 'green',
+   content: 'immediate attention required',
+},
+{
+  title: 'Safe URL',
+  color: 'green',
+  className: 'green',
+
+  content: 'immediate attention required',
+},
+]
 const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: '7/1', thisYear: 40, lastYear: 23, amt: 2400},
+      {name: '7/2', thisYear: 30, lastYear: 23, amt: 2210},
+      {name: '7/3', thisYear: 20, lastYear: 22, amt: 2290},
+      {name: '7/4', thisYear: 20, lastYear: 33, amt: 2000},
+      {name: '7/5', thisYear: 10, lastYear: 33, amt: 2181},
+      {name: '7/6', thisYear: 23, lastYear: 32, amt: 2500},
+      {name: '7/7', thisYear: 34, lastYear: 11, amt: 2100},
 ];
 const items = [
   { key: 'editorials', active: true, name: 'Editorials' },
@@ -22,11 +50,23 @@ class Home extends Component {
   state = { activeItem: 'report', activeItem2: 'shared' }
 
  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+ handleItemClick2 = (e, { name }) => this.setState({ activeItem2: name })
 
+ componentDidMount() {
+
+
+ }
   render() {
     const { isAuthenticated } = this.props.auth;
     const { activeItem, activeItem2 } = this.state
+    var dangerIndex =1;
+    if(activeItem2 === 'information2'){
+      dangerIndex = 2;
 
+    }else if(activeItem2 === 'information3'){
+        dangerIndex = 3;
+
+      }
     return (
       <div className="container">
         {
@@ -62,68 +102,130 @@ class Home extends Component {
                 </Menu.Item>
               </Menu.Menu>
             </Menu>
+            {activeItem === 'warning'&&
+          <div>
+            <Grid celled>
+                <Grid.Row>
+                  <Grid.Column width={4} className="borderNone">
+                    <Menu vertical>
+                            <Menu.Item>
+                              <Menu.Header>
+                                <Icon name='warning circle' color='red' />
+                                Danger
+                              </Menu.Header>
 
+                              <Menu.Menu>
+                                <Menu.Item name='information1' active={activeItem2 === 'information1'} onClick={this.handleItemClick2} />
+                                {/* <Menu.Item name='consumer' active={activeItem2 === 'consumer'} onClick={this.handleItemClick2} /> */}
+                              </Menu.Menu>
+                            </Menu.Item>
+
+                            <Menu.Item>
+                              <Menu.Header>
+                                <Icon name='warning sign' color='orange' />
+                                Warning
+                              </Menu.Header>
+
+                              <Menu.Menu>
+                                <Menu.Item name='information2' active={activeItem2 === 'information2'} onClick={this.handleItemClick2} />
+                                {/* <Menu.Item name='warning' active={activeItem2 === 'warning'} onClick={this.handleItemClick2} /> */}
+                                {/* <Menu.Item name='danger' active={activeItem2 === 'danger'} onClick={this.handleItemClick2} /> */}
+                              </Menu.Menu>
+                            </Menu.Item>
+
+                            <Menu.Item>
+                              <Menu.Header>
+                                <Icon name='check circle' color='green' />
+                                Safe
+                              </Menu.Header>
+                              <Menu.Menu>
+                                <Menu.Item name='information3' active={activeItem2 === 'information3'} onClick={this.handleItemClick2} />
+                                {/* <Menu.Item name='remove' active={activeItem2 === 'remove'} onClick={this.handleItemClick2} /> */}
+                              </Menu.Menu>
+                            </Menu.Item>
+
+                            <Menu.Item>
+                              <Menu.Header>Support</Menu.Header>
+
+                              <Menu.Menu>
+                                <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
+                                  E-mail Support
+                                </Menu.Item>
+
+                                <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
+                                  FAQs
+                                </Menu.Item>
+                              </Menu.Menu>
+                            </Menu.Item>
+                          </Menu>
+
+                  </Grid.Column>
+                  <Grid.Column width={12} className="borderNone">
+                            <Card.Group>
+
+                                {links.filter(x => x.danger===dangerIndex).map((t, index) => {
+
+                                  return (
+                                    <Card>
+                                    {t.danger === dangerIndex &&
+                                      <Card.Content>
+                                        <Card.Header>{index}</Card.Header>
+                                        <Card.Meta>
+                                          {t.danger == 1 &&
+                                            <Icon name='warning circle' color='red' />
+                                          }
+                                          {t.danger == 2 &&
+                                            <Icon name='warning sign' color='orange' />
+                                          }
+                                          {t.danger == 3 &&
+                                            <Icon name='warning sign' color='orange' />
+                                          }
+                                          <span className="country">
+                                            From
+                                            <span>
+                                              {t.origin}
+                                            </span>
+                                          </span>
+                                        </Card.Meta>
+                                        <Card.Description>{t.link}</Card.Description>
+                                     </Card.Content>
+                                    }
+                                  </Card>
+                                  )
+
+                                })}
+
+                             </Card.Group>
+
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </div>
+            }
+            {activeItem === 'report'&&
             <Segment>
-              <Menu vertical>
-                      <Menu.Item>
-                        <Menu.Header>Header A</Menu.Header>
+              <Segment>
+                <Icon name='dashboard' />
+                Overview
 
-                        <Menu.Menu>
-                          <Menu.Item name='enterprise' active={activeItem2 === 'enterprise'} onClick={this.handleItemClick} />
-                          <Menu.Item name='consumer' active={activeItem2 === 'consumer'} onClick={this.handleItemClick} />
-                        </Menu.Menu>
-                      </Menu.Item>
-
-                      <Menu.Item>
-                        <Menu.Header>Header B</Menu.Header>
-
-                        <Menu.Menu>
-                          <Menu.Item name='normal' active={activeItem2 === 'normal'} onClick={this.handleItemClick} />
-                          <Menu.Item name='warning' active={activeItem2 === 'warning'} onClick={this.handleItemClick} />
-                          <Menu.Item name='danger' active={activeItem2 === 'danger'} onClick={this.handleItemClick} />
-                        </Menu.Menu>
-                      </Menu.Item>
-
-                      <Menu.Item>
-                        <Menu.Header>Header C</Menu.Header>
-
-                        <Menu.Menu>
-                          <Menu.Item name='shared' active={activeItem2 === 'shared'} onClick={this.handleItemClick} />
-                          <Menu.Item name='remove' active={activeItem2 === 'remove'} onClick={this.handleItemClick} />
-                        </Menu.Menu>
-                      </Menu.Item>
-
-                      <Menu.Item>
-                        <Menu.Header>Support</Menu.Header>
-
-                        <Menu.Menu>
-                          <Menu.Item name='email' active={activeItem === 'email'} onClick={this.handleItemClick}>
-                            E-mail Support
-                          </Menu.Item>
-
-                          <Menu.Item name='faq' active={activeItem === 'faq'} onClick={this.handleItemClick}>
-                            FAQs
-                          </Menu.Item>
-                        </Menu.Menu>
-                      </Menu.Item>
-                    </Menu>
+                <LineChart width={600} height={300} data={data}
+                margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+           <XAxis dataKey="name"/>
+           <YAxis/>
+           <CartesianGrid strokeDasharray="3 3"/>
+           <Tooltip/>
+           <Legend />
+           <Line type="monotone" dataKey="thisYear" stroke="#8884d8" activeDot={{r: 8}}/>
+           <Line type="monotone" dataKey="lastYear" stroke="#82ca9d" />
+          </LineChart>
+              </Segment>
+              <Accordion panels={panels} className="customerAccordion" styled />
 
             </Segment>
-          <Segment>
-            <Icon name='dashboard' />
-            Overview
 
-            <LineChart width={600} height={300} data={data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-       <XAxis dataKey="name"/>
-       <YAxis/>
-       <CartesianGrid strokeDasharray="3 3"/>
-       <Tooltip/>
-       <Legend />
-       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
-       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
-          </Segment>
+            }
+
+
           </div>
       </div>
     );
---
+++ src/linkData.js
@@ -0,0 +1,102 @@
+var links =[{
+  "id": 1,
+  "link": "https://fastcompany.com/donec/quis/orci.jpg",
+  "danger": 2,
+  "origin": "Portugal"
+}, {
+  "id": 2,
+  "link": "http://fotki.com/ultrices/enim/p",
+  "danger": 2,
+  "origin": "Iraq"
+}, {
+  "id": 3,
+  "link": "http://ehow.com/donec.html?",
+  "danger": 1,
+  "origin": "Palestinian Territory"
+}, {
+  "id": 4,
+  "link": "https://diigo.com/aenean/fermentum/donec/ut/mauris/eget/",
+  "danger": 3,
+  "origin": "Kenya"
+}, {
+  "id": 5,
+  "link": "http://diigo.com/fermentum/donec/",
+  "danger": 1,
+  "origin": "Russia"
+}, {
+  "id": 6,
+  "link": "https://hubpages.com/parturient/montes",
+  "danger": 2,
+  "origin": "Saudi Arabia"
+}, {
+  "id": 7,
+  "link": "http://cnet.com/enim/leo/rhoncus/sed/", "danger": 3,
+  "origin": "Russia"
+}, {
+  "id": 8,
+  "link": "http://jigsy.com/parturient/montes/",
+  "danger": 2,
+  "origin": "Indonesia"
+}, {
+  "id": 9,
+  "link": "https://chicagotribune.com/",
+  "danger": 3,
+  "origin": "Democratic Republic of the Congo"
+}, {
+  "id": 10,
+  "link": "https://ucoz.ru/in/hac/habitasse/",
+   "danger": 1,
+  "origin": "Russia"
+}, {
+  "id": 11,
+  "link": "http://123-reg.co.uk/erat/quisque/erat",
+  "danger": 1,
+  "origin": "Croatia"
+}, {
+  "id": 12,
+  "link": "https://yale.edu/duis/bibendume",
+  "danger": 3,
+  "origin": "Brazil"
+}, {
+  "id": 13,
+  "link": "http://ezinearticles.com/ut/",
+  "danger": 3,
+  "origin": "United States"
+}, {
+  "id": 14,
+  "link": "http://cocolog-nifty.com/",
+  "danger": 2,
+  "origin": "Bosnia and Herzegovina"
+}, {
+  "id": 15,
+  "link": "http://desdev.cn/blandit/nam",
+  "danger": 2,
+  "origin": "China"
+}, {
+  "id": 16,
+  "link": "http://sfgate.com/pulvinar/lobortis.png?"
+  ,"danger": 1,
+  "origin": "Nepal"
+}, {
+  "id": 17,
+  "link": "https://java.com/tristique/est/",
+  "danger": 3,
+  "origin": "Estonia"
+}, {
+  "id": 18,
+  "link": "https://meetup.com/varius/ut/blandit/",
+  "danger": 2,
+  "origin": "Brazil"
+}, {
+  "id": 19,
+  "link": "https://trellian.com/congue/risus/"
+  ,"danger": 2,
+  "origin": "Norway"
+}, {
+  "id": 20,
+  "link": "https://nhs.uk/quis/turpis/sed.png?m",
+  "danger": 2,
+  "origin": "Honduras"
+}];
+
+export {links};
